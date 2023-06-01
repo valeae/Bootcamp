b96c34b6b8a5eeb6aaba229ee8bf5e73b54e53c6
valeae <vaescobar95.ve@gmail.com>
2023-06-01 10:59:29 -0500

    1.6 Unicafe Step 1

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1> Give Feedback </h1>
+      <button onClick = {() => setGood(good + 1)}>Good</button>
+			<button onClick = {() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick = {() => setBad(bad + 1)}>Bad</button>
+
+      <h1> Statistics </h1>
+      <h3>Good: {good}</h3>
+			<h3>Neutral: {neutral}</h3>
+			<h3>Bad: {bad}</h3>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
